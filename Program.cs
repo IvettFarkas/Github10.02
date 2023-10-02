03e3bdbbc54168db932cb4e497b821b5febb4fbb
B19FarkasI <B19FarkasI@204-2>
2023-10-02 12:49:48 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Github10._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Ivett";
+            Console.WriteLine();
+        }
+    }
+}
